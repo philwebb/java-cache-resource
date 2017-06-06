338eb5f62924ef73dd94cce6ea36c9db4b6d9203
Phillip Webb <pwebb@pivotal.io>
2017-06-06 15:27:08 -0700

    Hacking

--- samples/gradle/ci/scripts/build.sh
+++ samples/gradle/ci/scripts/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-set -x
 ln -fs $(pwd)/java-cache/gradle ~/.gradle
 cd git-repo/samples/gradle
 ./gradlew --offline build
--- samples/maven/ci/scripts/build.sh
+++ samples/maven/ci/scripts/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-set -x
 ln -fs $(pwd)/java-cache/m2 ~/.m2
 cd git-repo/samples/maven
 ./mvnw -o clean package
