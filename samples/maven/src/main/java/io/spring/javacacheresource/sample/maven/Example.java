42e6e6901b3d1370c56e2e708fd30cec22f458d7
Phillip Webb <pwebb@pivotal.io>
2017-06-06 13:11:13 -0700

    Samples

---
+++ samples/maven/src/main/java/io/spring/javacacheresource/sample/maven/Example.java
@@ -0,0 +1,11 @@
+package io.spring.javacacheresource.sample.maven;
+
+import org.springframework.util.StringUtils;
+
+public class Example {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.arrayToCommaDelimitedString(args));
+	}
+
+}
